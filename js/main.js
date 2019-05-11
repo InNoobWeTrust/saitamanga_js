1da2ffdefea1f5e7c7a7679872b4cb608afda5fa
InNoobWeTrust <forbidden.gun@gmail.com>
2019-05-11 22:43:46 +0700

    Fixes suggested by eslint

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-import request from './request.js'
+import request from './request.js';
 const onDone = (html) => {
-  document.getElementById("root").innerHTML =
-    `<iframe id="frame" style="border:none;margin:none;width:97vw;height:96vh">${html}<//iframe>`;
+  document.getElementById('root').innerHTML =
+    `<iframe id="frame" style="border:none;margin:none;width:97vw;height:96vh">
+    ${html}
+    <//iframe>`;
   const doc = document.getElementById('frame').contentWindow.document;
   doc.open();
   doc.write(html);
   doc.close();
-}
-request("GET", "http://hocvientruyentranh.net", onDone);
+};
+request('GET', 'http://hocvientruyentranh.net', onDone);
