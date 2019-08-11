2e97df8d50bfe9ebfb847cf9b074a57529456b6d
InNoobWeTrust <forbidden.gun@gmail.com>
2019-08-11 12:27:15 +0700

    Enable debug tools by default

--- js/mobile_log.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  var src = '//cdn.jsdelivr.net/npm/eruda';
-  if (!/eruda=true/.test(window.location) && localStorage.getItem('active-eruda') != 'true')
-    return;
-  document.write(`<script src=${src}></script>`);
-  document.write('<script>eruda.init();</script>');
-})();
