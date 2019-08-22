de3ea1c76ae31704d739c121c08c3f337e85ffa0
InNoobWeTrust <forbidden.gun@gmail.com>
2019-08-23 00:38:34 +0700

    Test basic render

--- js/main.js
+++ js/main.js
@@ -1,54 +1,55 @@
-import request from './request.js'
+import request from './request.js';
 
 const onDoneShow = async (html) => {
-  document.getElementById("root").innerHTML =
-    `<iframe id="frame" style="border:none;margin:none;width:97vw;height:96vh">${html}<//iframe>`;
-  const doc = document.getElementById('frame').contentWindow.document;
-  doc.open();
-  doc.write(html);
-  doc.close();
-}
+  const dom = document.createElement('html');
+  dom.innerHTML = html;
+  document.getElementById('root').innerHTML = await galleryRender(dom);
+};
+
+const galleryRender = async (dom) => {
+  const titleParser = selectParser(
+    '.table.table-hover>tbody>tr>td:nth-child(1)>a'
+  );
+  const titles = await titleParser(dom);
+  const lTitles = titles.map((t) => `<p>${t}</p>`).join('');
+  return lTitles;
+};
 
 Element.prototype.getAttr = function(attr) {
   let res;
-  switch(attr) {
-    case undefined:
-    case null:
-    case "":
-    case "text":
-      res = this.innerText;
-      break;
-    default:
-      res = this.getAttribute(attr);
-      break;
+  switch (attr) {
+  case undefined:
+  case null:
+  case '':
+    res = this.innerText;
+    break;
+  default:
+    res = this.getAttribute(attr);
+    break;
   }
   return res;
-}
+};
 
-const query = (selectAll = true) => dom => selector => {
+const querior = (selector, selectAll = true) => async (elem) => {
   if (selectAll) {
-    return dom.querySelectorAll(selector);
+    return elem.querySelectorAll(selector);
   } else {
-    return [dom.querySelector(selector)];
+    return [elem.querySelector(selector)];
   }
-}
+};
 
-const selectParser = (selector, attr, selectAll = true) => async (html) => {
-  const dom = document.createElement('html');
-  dom.innerHTML = html;
-  const nodeListTitles = query(selectAll)(dom)(selector);
-  const titles = [...nodeListTitles].map(title => title.getAttr(attr));
-  console.log(titles);
-  return titles;
-}
+const selectParser = (selector, attr, selectAll = true) => async (elem) => {
+  const nodeListData = await querior(selector, selectAll)(elem);
+  const data = [...nodeListData].map((e) => e.getAttr(attr));
+  return data;
+};
 
 request(
-  "GET",
-  "https://hocvientruyentranh.net/truyen/all",
-  html => Promise.all(
+  'GET',
+  'https://hocvientruyentranh.net/truyen/all',
+  (html) => Promise.all(
     [
-      selectParser(".table.table-hover>tbody>tr>td:nth-child(1)>a", "text"),
       onDoneShow,
-    ].map(cb => cb(html))
+    ].map((cb) => cb(html))
   )
 );
--- js/proxy.js
+++ js/proxy.js
@@ -1,12 +1,10 @@
-(function() {
+(function(...args) {
   const corsApiHost = 'cors-anywhere.herokuapp.com';
   const corsApiUrl = 'https://' + corsApiHost + '/';
-  const slice = [].slice;
   const origin = window.location.protocol + '//' + window.location.host;
   const open = XMLHttpRequest.prototype.open;
   XMLHttpRequest.prototype.open = function() {
-    const args = slice.call(arguments);
-    const targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+    const targetOrigin = /^https?:\/\/([^/]+)/i.exec(args[1]);
     if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
       targetOrigin[1] !== corsApiHost) {
       args[1] = corsApiUrl + args[1];
